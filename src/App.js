3274a8f129abe5659e3f035599ea3e46b13c9cb4
Cam Jackson <camjackson89@gmail.com>
2017-05-11 08:35:57 +1000

    Add the list of services/events

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { eventTypes } from './services';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h2>Welcome to React</h2>
+        <h2>Rabble Rouser Event Docs</h2>
         <p>To get started, edit <code>src/App.js</code> and save to reload.</p>
+        <ol>
+          {eventTypes.map(eventType => (
+            <li key={eventType}>{eventType}</li>
+          ))}
+        </ol>
       </div>
     );
   }
---
+++ src/services.js
@@ -0,0 +1,43 @@
+export const services = {
+  signup: {
+    consumes: ['branch-created', 'branch-removed', 'branch-edited'],
+    publishes: ['member-registered', 'send-email'],
+  },
+  admin: {
+    consumes: [
+      'member-registered', 'member-removed', 'member-edited',
+      'branch-created', 'branch-removed', 'branch-edited',
+      'group-created', 'group-removed', 'group-edited',
+      'admin-created', 'admin-removed', 'admin-edited',
+    ],
+    publishes: [
+      'member-removed', 'member-edited',
+      'branch-created', 'branch-removed', 'branch-edited',
+      'group-created', 'group-removed', 'group-edited',
+      'admin-created', 'admin-removed', 'admin-edited',
+    ],
+  },
+  mailer: {
+    consumes: ['send-email'],
+    publishes: ['email-sent', 'email-failed'],
+  },
+  'group-mailer': {
+    consumes: [
+      'member-registered', 'member-removed', 'member-edited',
+      'group-created', 'group-removed', 'group-edited',
+    ],
+    publishes: ['send-email'],
+  }
+};
+
+export const eventTypes = [];
+const pushEventType = eventType => {
+  if (!eventTypes.includes(eventType)) {
+    eventTypes.push(eventType);
+  }
+}
+Object.keys(services).forEach(serviceName => {
+  services[serviceName].consumes.forEach(pushEventType);
+  services[serviceName].publishes.forEach(pushEventType);
+});
+eventTypes.sort();
